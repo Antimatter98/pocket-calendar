c3d45894ed7c72562afc108e655e9b8d9e2eae16
Nishant Tilve <nishanttilve@gmail.com>
2020-08-08 20:07:30 +0530

    fixed https 🔒

--- middlewares/forceHTTPS.js
+++ middlewares/forceHTTPS.js
@@ -1,14 +1,10 @@
 var express = require("express");
 require("dotenv").config();
 
-var forceHTTPS = (req, res, next) => {
+module.exports = function (req, res, next) {
   if (process.env.NODE_ENV === "production") {
     if (req.headers["x-forwarded-proto"] !== "https") {
       return res.redirect("https://" + req.headers.host + req.url);
     } else return next();
   } else return next();
-};
-
-module.exports = {
-  forceHTTPS,
 };
